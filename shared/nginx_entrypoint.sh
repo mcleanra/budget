072a7a00f0f9358ae1eb68ecc5a19426b9df6372
Ryan McLean <mcleanra@gmail.com>
2019-12-29 15:15:41 +0100

    use a full config file and start nginx with it

--- shared/nginx_entrypoint.sh
+++ shared/nginx_entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 export DNS_SERVER=$(/get_dns.sh);
 envsubst '${DNS_SERVER}' < /etc/nginx/conf.d/default.template.conf > /etc/nginx/conf.d/default.conf;
 
+echo "Starting nginx..."
+exec $(which nginx) -c /etc/nginx/conf.d/default.conf -g "daemon off;"
+
 exec "$@"
