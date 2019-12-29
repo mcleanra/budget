bd835f8cf73691590e3ddabc41a4d4f18f2f439a
Ryan McLean <mcleanra@gmail.com>
2019-12-29 14:57:25 +0100

    don't cat the modified config

--- shared/nginx_entrypoint.sh
+++ shared/nginx_entrypoint.sh
@@ -3,6 +3,5 @@ set -e
 
 export DNS_SERVER=$(/get_dns.sh);
 envsubst '${DNS_SERVER}' < /etc/nginx/conf.d/default.template.conf > /etc/nginx/conf.d/default.conf;
-cat /etc/nginx/conf.d/default.conf;
 
 exec "$@"
