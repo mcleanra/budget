f98663c43cbaad96f653a617e778ceb69f2f3a17
Ryan McLean <mcleanra@gmail.com>
2019-12-29 14:13:22 +0100

    renamed file for syntax highlighting

--- shared/nginx_entrypoint.sh
+++ shared/nginx_entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 export DNS_SERVER=$(/get_dns.sh);
-envsubst '${DNS_SERVER}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf;
+envsubst '${DNS_SERVER}' < /etc/nginx/conf.d/default.template.conf > /etc/nginx/conf.d/default.conf;
 cat /etc/nginx/conf.d/default.conf;
 
 exec "$@"
